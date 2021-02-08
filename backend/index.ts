1484d04ddb4b2450bde17ac90faafb2df2db7b9d
Ben Ferch <ben@benferch.de>
2021-02-08 02:52:31 +0100

    posts are inserted into database

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
 import fetch from 'node-fetch';
 require('dotenv').config();
@@ -12,36 +12,71 @@ const url = `https://www.reddit.com/r/${subs.join('+')}/top.json?limit=${
 }`;
 
 const app = express();
-
 const port = 8081;
-/* 
-mongoose.connect(
-	`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017`,
-	{ useNewUrlParser: true, useUnifiedTopology: true }
-); 
-*/
+
+mongoose
+	.connect(
+		`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@db:27017/bot?authSource=admin`,
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.catch((e) => console.error(e));
+
+mongoose.connection.on('error', (err) => {
+	console.error(err);
+});
 
 const PostSchema = new mongoose.Schema({
-	id: Number,
+	id: String,
 	title: String,
 	author: String,
-	ups: Number,
+	upvotes: Number,
 	imageUrl: String,
 	timeAdded: Date,
 	posted: Boolean,
 });
 
-const Post = mongoose.model('Post', PostSchema);
+const PostModel = mongoose.model('Post', PostSchema);
 
 function getReddit() {
 	fetch(url)
 		.then((res) => res.json())
-		.then((data) => console.log(data));
+		.then((data) => {
+			data.data.children.forEach(
+				(el: {
+					data: {
+						id: String;
+						title: String;
+						author: String;
+						ups: Number;
+						url: String;
+					};
+				}) => {
+					const Post = new PostModel({
+						id: el.data.id,
+						title: el.data.title,
+						author: el.data.author,
+						upvotes: el.data.ups,
+						imageUrl: el.data.url,
+						timeAdded: Date.now(),
+						posted: false,
+					});
+					PostModel.find({ id: el.data.id }, function (err, docs) {
+						if (!docs.length) {
+							Post.save().then(() => console.log('post'));
+						} else {
+							console.error(
+								`Post with id ${el.data.id} exists \nError: ${err}`
+							);
+						}
+					});
+				}
+			);
+		});
 }
 
-setTimeout(function () {
-	getReddit();
-}, config.interval);
+getReddit();
+
+// setTimeout(function () {}, config.interval);
 
 app.get('/', (res: express.Response) => {
 	res.send('Hello World!');
