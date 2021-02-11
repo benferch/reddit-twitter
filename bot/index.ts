f8cdca949e54fc497495e8e1a9d42edcff5f33c4
Ben Ferch <ben@benferch.de>
2021-02-11 02:39:49 +0100

    bot gets posts from database

--- bot/index.ts
+++ bot/index.ts
@@ -1,2 +1,12 @@
 import fetch from 'node-fetch';
 import { config } from './config';
+
+const url = 'http://backend:8081/';
+
+function getPosts() {
+	return fetch(`${url}getUnposted`)
+		.then((res) => res.json())
+		.then((data) => {
+			return data[0];
+		});
+}
