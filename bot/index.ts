12d5828b56c967b8b0d224471b430b68907dda8d
Ben Ferch <ben@benferch.de>
2021-02-12 03:06:16 +0100

    added tsconfig to bot - added secrets to bot and fixed problem where tweet had to run twice to fix resizing of image

--- bot/index.ts
+++ bot/index.ts
@@ -1,35 +1,42 @@
 import { config } from './config';
 import { timestamp } from './utils/DateFunctions';
 import fetch from 'node-fetch';
-import * as sharp from 'sharp';
+import sharp from 'sharp';
 import { createWriteStream } from 'fs';
 require('dotenv').config();
 
 const URL = 'http://backend:8081/';
+const SECRET = {
+	CONSUMER_KEY: process.env.CONSUMER_KEY,
+	CONSUMER_SECRET: process.env.CONSUMER_SECRET,
+	ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+	ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+};
 
-function tweet() {
+async function tweet() {
 	console.log(`Next time tweeting: ${timestamp(config.interval)}.`);
-	fetch(`${URL}getUnposted`)
+	await fetch(`${URL}getUnposted`)
 		.then((res) => res.json())
-		.then((data) => {
+		.then(async (data) => {
 			const id = data[0].id,
 				title = data[0].title,
 				author = data[0].author,
 				imageUrl = data[0].imageUrl,
 				postUrl = data[0].postUrl;
-			fetch(imageUrl).then((res) => {
+			await fetch(imageUrl).then((res) => {
 				const dest = createWriteStream('./img/post.png');
-				res.body.pipe(dest);
+				res.body.pipe(dest).on('close', () =>
+					sharp('./img/post.png')
+						.resize(1000)
+						.toFile('./img/out.png', (err, info) => {
+							if (err) {
+								console.error(err);
+							} else {
+								console.info(info);
+							}
+						})
+				);
 			});
-			sharp('./img/post.png')
-				.resize(1000)
-				.toFile('./img/out.png', (err, info) => {
-					if (err) {
-						console.error(err);
-					} else {
-						console.info(info);
-					}
-				});
 		});
 	setTimeout(tweet, config.interval);
 }
