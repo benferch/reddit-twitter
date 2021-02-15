7723e107c36ef947ce0ebc9bcc26653881aeac61
Ben Ferch <ben@benferch.de>
2021-02-16 00:06:32 +0100

    updated example configs

--- backend/example.config.ts
+++ backend/example.config.ts
@@ -9,12 +9,12 @@
 	text = Should text posts be included | default: false
 	requestInterval = Time between requests in milliseconds | default: 1 hour (3600000) 
 	deleteInterval = Time between deletion of posts in database | default: 1 day (86400000)
-	limit = Amount of posts gathered | default: 50
+	limit = Amount of posts gathered | default: 100 (maximum: 100)
 */
 
 export const config = {
 	text: false,
 	requestInterval: 3600000,
 	deleteInterval: 86400000,
-	limit: 50,
+	limit: 100,
 };
