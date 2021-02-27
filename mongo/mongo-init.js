2934a18f418bef9fe250e995ce9ea218a92390c8
Ben Ferch <ben@benferch.de>
2021-02-27 22:44:57 +0100

    updated seed script for mongodb

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,3 +1,14 @@
 db.auth('root', 'root');
 
-db = db.getSiblingDB('bot');
+db = db.getSiblingDB('admin');
+
+db.createUser({
+	user: 'root',
+	pwd: 'root',
+	roles: [
+		{
+			role: 'userAdminAnyDatabase',
+			db: 'admin',
+		},
+	],
+});
